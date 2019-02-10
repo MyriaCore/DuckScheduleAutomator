6078b7f6630e97b5549cda085d74e53203a014c8
myriacore <development@myriacore.me>
2019-02-10 16:13:28 -0500

    Renamed `convert_date` to `convert_time` in `date_util.py`

--- src/date_util.py
+++ src/date_util.py
@@ -1,12 +1,10 @@
+# TODO: consider using `time` class instead of `datetime` here.
 from datetime import datetime
 
-
-
-def convert_date(time):
+def convert_time(time):
     """
     `time` is a timestamp string ending in Z (Ex: '11:50:00Z')
     """
-
     return datetime.strptime(time, '%H:%M:%S' + 'Z').time()
 
 
@@ -14,18 +12,10 @@ def get_hour(time):
     """
     `time` is a timestamp string ending in Z (Ex: '11:50:00Z')
     """
-
     return datetime.strptime(time, '%H:%M:%S' + 'Z').hour
 
 def get_minutes(time):
     """
     `time` is a timestamp string ending in Z (Ex: '11:50:00Z')
     """
-
     return datetime.strptime(time, '%H:%M:%S' + 'Z').minute
-
-
-
-
-
-
