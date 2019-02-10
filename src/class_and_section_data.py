b7546634d0c9195f747b168da43b4de84823634a
myriacore <development@myriacore.me>
2019-02-10 15:33:45 -0500

    Started implementing the selective copy todo in the clean function in `class_and_section_data.py`

--- src/class_and_section_data.py
+++ src/class_and_section_data.py
@@ -53,18 +53,25 @@ def __clean__(section):
         "X": "McLean Hall, Hoboken, NJ 07030"
     }
 
-    clean_section = deepcopy(section) # TODO: find a way to selectively copy key val pairs that can stay as strings
+    clean_section = {}
     clean_section["maxEnrollment"] = int(section["maxEnrollment"])
     clean_section["currentEnrollment"] = int(section["currentEnrollment"])
+    if type(section["daysTimeLocation"]) is dict:
+        clean_section["daysTimeLocation"]["day"] = weekdays[section["daysTimeLocation"]["day"]]
+    elif type(section["daysTimeLocation"]) is list:
+        pass # TODO: implement
+    else:
+        pass #TODO: implement
+
+
+    # clean_section = deepcopy(section) # TODO: find a way to selectively copy key val pairs that can stay as strings
+
     # TODO: Find a way to add a "roomloc" key that just has like "NB 102" as a val
     # TODO: make sure we handle this if it's a list of daytimelocations
 
-    clean_section["daysTimeLocation"]["day"] = weekdays[section["daysTimeLocation"]["day"]]
 
     return clean_section
 
 
 
 # TODO: function to read in all data as `DuckSchedule` objects.
-
-
